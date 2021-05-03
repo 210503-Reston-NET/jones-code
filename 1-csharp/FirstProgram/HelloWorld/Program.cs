70b3f908f15147a5dc97f4469550a71d3dd2c074
Kagan Jones <32104869+Gwigs@users.noreply.github.com>
2021-05-03 17:36:52 -0400

    Added my first program code

---
+++ 1-csharp/FirstProgram/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+/* namimng conventions:
+  in .net, we use PascalCase. Name artifacts like this: EveryFirstLetterIsCapitalized.
+  we use camelCase for naming fields.
+*/
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
